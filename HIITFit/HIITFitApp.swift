e2b69b4a461289e7d56c70eeeb084914b6dc5ab1
Vladimir Solovyov <leilyngale@icloud.com>
2025-04-29 16:19:30 +0800

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by VS on 2025/4/29.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
