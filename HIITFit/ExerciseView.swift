a046306b959be5275561215b56fe37db4ca1e395
Vladimir Solovyov <leilyngale@icloud.com>
2025-04-29 16:50:09 +0800

    Extract tab views into separate View files, group the files into a folder

---
+++ HIITFit/ExerciseView.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseView.swift
+//  HIITFit
+//
+//  Created by VS on 2025/4/29.
+//
+
+import SwiftUI
+
+struct ExerciseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseView()
+}
---
+++ HIITFit/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  HIITFit
+//
+//  Created by VS on 2025/4/29.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
